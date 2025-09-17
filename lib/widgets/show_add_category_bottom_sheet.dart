fa1768048a69e1303316a766a8a0d96b2481f52c
Safwan <safwanmv007@gmail.com>
2025-09-17 11:11:31 +0530

    feat: allow users to add new categories

---
+++ lib/widgets/show_add_category_bottom_sheet.dart
@@ -0,0 +1,10 @@
+// import 'package:flutter/material.dart';
+
+// class showAddCategoryBottomSheet extends StatelessWidget {
+//   const showAddCategoryBottomSheet({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SnackBar(content:);
+//   }
+// }
