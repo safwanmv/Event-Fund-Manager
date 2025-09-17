936ab498abb398304e1dc82119db95d553c0842b
Safwan <safwanmv007@gmail.com>
2025-09-17 11:10:00 +0530

    feat(ui):list top 4 transaction on top banner

--- lib/widgets/homecreen_top_banner/homescreen_top_banner.dart
+++ lib/widgets/homecreen_top_banner/homescreen_top_banner.dart
@@ -1,5 +1,10 @@
+import 'dart:math' as math;
+
+import 'package:expense_tracker/db/transaction_db/transaction_db.dart';
+import 'package:expense_tracker/models/categroy/category_model.dart';
 import 'package:expense_tracker/theme/colors/color.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomescreenTopBanner extends StatelessWidget {
   final Color? color;
@@ -7,89 +12,107 @@ class HomescreenTopBanner extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double initialOffset=312*2;
-    final ScrollController scrollController=ScrollController(initialScrollOffset: initialOffset);
-    return SizedBox(
-      height: 230,
-      child: GridView.builder(
-        controller: scrollController,
-        scrollDirection: Axis.horizontal,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 1,
-          mainAxisExtent: 330,
+    final double initialOffset = 312 * 2;
+    final ScrollController scrollController = ScrollController(
+      initialScrollOffset: initialOffset,
+    );
+    return ValueListenableBuilder(
+      valueListenable: TransactionDb.instance.transactionListNotifer,
+      builder: (context, newList, _) {
+        final incomes = newList
+            .where((i) => i.type == CategoryType.income)
+            .toList();
+        if (incomes.isEmpty) {
+          return const Center(child: Text("data"));
+        }
+        incomes.sort((a, b) => b.amount.compareTo(a.amount));
+        final topIncomes=incomes.take(4).toList();
 
-        ),
+        // final double highest=topIncomes.map((i)=>i.amount).fold(0.0, (p,e)=>math.max(p,e));
+        return SizedBox(
+          height: 230,
+          child: GridView.builder(
+            controller: scrollController,
+            scrollDirection: Axis.horizontal,
 
-        itemBuilder: (context, index) {
-          
-          return Container(
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: topBannerColors[index],
-              borderRadius: BorderRadius.circular(35),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              mainAxisExtent: 330,
             ),
-            height: 80,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            itemBuilder: (context, index) {
+              final value = topIncomes[index];
+              final formattedDate = DateFormat('dd-MM-yyyy').format(value.date);
+              return Container(
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: topBannerColors[index % topBannerColors.length],
+                  borderRadius: BorderRadius.circular(35),
+                ),
+                height: 80,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Salary",
-                              style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w500,
-                              ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  value.name,
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Text(
+                                  "₹${value.amount}",
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.w700,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              ],
                             ),
-                            Text(
-                              "₹45,000",
-                              style: TextStyle(
-                                fontSize: 38,
-                                fontWeight: FontWeight.w700,
-                                overflow: TextOverflow.ellipsis,
-                              ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  value.type.toString(),
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                Text(
+                                  formattedDate,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
                             ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text(
-                              "Deposit",
-                              style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                            Text(
-                              '01-03-2025',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
-            ),
-          );
-        },
-        itemCount: 4,
-      ),
+                ),
+              );
+            },
+            itemCount: topIncomes.length,
+
+          ),
+        );
+      },
     );
   }
 }
